7e3459c9bc242fb17d6dc121b783c2afc939831f
Benjamin <17wilsb@olab.org.uk>
2023-11-08 14:53:29 +0000

    Power mod

--- DHKEA.py
+++ DHKEA.py
@@ -1,3 +1,31 @@
 def power_mod(A, n, p):
 
-    pass
+    total = 1
+
+    operations = ""
+
+    while n > 0:
+
+        if n % 2 == 1:
+
+            operations += "1"
+            n -= 1
+        
+        else:
+            
+            operations += "0"
+            n //= 2
+
+    for op in reversed(operations):
+
+        if op == "0":
+
+            total *= total
+            total %= p
+        
+        else:
+
+            total *= A
+            total %= p
+    
+    return total
